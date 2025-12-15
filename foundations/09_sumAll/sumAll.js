41c54d903f88dfb2dfaee607638da98fa1dd30fb
Dennis Ignacio <dsvignacio@gmail.com>
2025-12-15 15:53:29 +0800

    done: 09_sumAll
    All tests pass.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  args.sort();
+  
+  for (const arg in args) {
+    if ((args[arg] < 0) || !(Number.isInteger(args[arg]))) {
+      return "ERROR";
+    }
+  }
+
+  let total = 0;
+  for (let i = args[0]; i <= args[1]; i++) {
+    total += i;
+  }
+  return total;
 
 };
 
