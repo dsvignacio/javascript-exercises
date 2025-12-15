5a5c745443cfa3b195d024dec9794d0794148731
Dennis Ignacio <dsvignacio@gmail.com>
2025-12-15 16:31:58 +0800

    done: 11_tempConversion
    All tests pass.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(valueFahrenheit) {
+  let valueCelsius = ((valueFahrenheit - 32) * (5/9));
+  let rounded = Math.round(valueCelsius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(valueCelsius) {
+  const valueFahrenheit = ((valueCelsius * (9/5)) + 32);
+  let rounded = Math.round(valueFahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
