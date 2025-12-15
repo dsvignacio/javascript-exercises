c1f9ca5c4a81f314bb179c96915d8c2d15f724c8
Dennis Ignacio <dsvignacio@gmail.com>
2025-12-15 11:02:40 +0800

    done: 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let reversed = "";
+  for (let i = -1; i > -(word.length); i--) {
+    reversed += word[i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
